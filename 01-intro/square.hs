a0cefa1dcb255f1fc2a695ff7178ed658a6c12c9
Yuriy Syrovetskiy <cblp@cblp.su>
2020-09-26 19:41:31 +0300

    Add 01-intro

---
+++ 01-intro/square.hs
@@ -0,0 +1,22 @@
+square x = x * x
+
+-- a x² + b x + c = 0
+solveSquareEquation a b c =
+  -- D = b² - 4 a c
+  -- x₁,₂ = (- b ± √D) / 2 a
+  let d = b * b - 4 * a * c
+  in
+    if a == 0 then
+      _
+    else if d < 0 then
+      []
+    else
+      -- [ (- b - sqrt d) / (2 * a)
+      -- , (- b + sqrt d) / (2 * a)
+      -- ]
+      [ ((- b) ± sqrt d) / (2 * a)
+      | (±) <- [(-), (+)]
+      ]
+
+main =
+  print (solveSquareEquation 1 (-2) 1)
