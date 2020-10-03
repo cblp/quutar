3ade1d819feb83e28e16e175a2e00fae34f6bb4e
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-04 00:04:53 +0300

    02: Add show for all variants

--- 02-types/types.hs
+++ 02-types/types.hs
@@ -90,20 +90,26 @@ data Expr
   = Number Double
   | Var
   | Plus Expr Expr
-  -- | Mul Expr Expr
-  -- | Sub Expr Expr
-  -- | Div Expr Expr
-  -- | Sin Expr
-  -- | Cos Expr
-  -- | Pow Expr Integer
+  | Mul Expr Expr
+  | Sub Expr Expr
+  | Div Expr Expr
+  | Sin Expr
+  | Cos Expr
+  | Pow Expr Integer
   deriving (Eq)
 
 instance Show Expr where
   show expr =
     case expr of
-      Number num         -> show num
-      Var                -> "x"
-      Plus   expr1 expr2 -> show expr1 ++ " + " ++ show expr2
+      Number n   -> show n
+      Var        -> "x"
+      Plus e1 e2 -> "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
+      Mul  e1 e2 -> "(" ++ show e1 ++ " * " ++ show e2 ++ ")"
+      Sub  e1 e2 -> "(" ++ show e1 ++ " - " ++ show e2 ++ ")"
+      Div  e1 e2 -> "(" ++ show e1 ++ " / " ++ show e2 ++ ")"
+      Sin e      -> "(sin " ++ show e ++ ")"
+      Cos e      -> "(cos " ++ show e ++ ")"
+      Pow e n    -> "(" ++ show e ++ " ^ " ++ show n ++ ")"
 
 showFullExpr :: Expr -> String
 showFullExpr f = "f x = " ++ show f
@@ -123,6 +129,9 @@ eval expr x =
     Plus expr1 expr2 -> eval expr1 x + eval expr2 x
     Number n         -> n
     Var              -> x
+
+    -- вот это надо убрать в соответствующем варианте:
+    _                -> undefined
 
 -- find :: [a] -> a -> Maybe Int
 
