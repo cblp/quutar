f08a5b5c4962292fffdeb501abc9dc6693d36346
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-10 19:07:52 +0300

    Add `stack test` for 03rd task

--- 03-testing/test/Test.hs
+++ 03-testing/test/Test.hs
@@ -6,8 +6,7 @@ import           Square           (solveSquareEquation)
 main :: IO ()
 main =
   defaultMain $
-    testGroup
-      "solveSquareEquation"
+    testGroup "solveSquareEquation"
       [ testCase "1 2 1" $
           solveSquareEquation 1 2 1 @?= [-1, -1]
       , testCase "1 (-2) 1" $
