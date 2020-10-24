ceef7ab3636b83aca2c3980010b10dc29d222625
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-24 16:04:33 +0300

    Add tasty-quickcheck and property tests

--- 03-testing/test/Test.hs
+++ 03-testing/test/Test.hs
@@ -1,14 +1,29 @@
-import           Test.Tasty       (defaultMain, testGroup)
-import           Test.Tasty.HUnit (testCase, (@?=))
+import           Test.Tasty            (defaultMain, testGroup)
+import           Test.Tasty.HUnit      (assertBool, testCase, (@?=))
+import           Test.Tasty.QuickCheck (conjoin, counterexample, label,
+                                        testProperty, (===), (==>))
 
-import           Square           (solveSquareEquation)
+import           Square                (solveSquareEquation)
 
 main :: IO ()
 main =
   defaultMain $
     testGroup "solveSquareEquation"
-      [ testCase "1 2 1" $
-          solveSquareEquation 1 2 1 @?= [-1, -1]
-      , testCase "1 (-2) 1" $
-          solveSquareEquation 1 (-2) 1 @?= [1, 1]
+      [ testProperty "any a, b, c" $
+          -- ∀ a b c : Double .
+          \a b c ->
+            a /= 0 ==>
+              let roots = solveSquareEquation a b c
+              in  -- ∀ x : solveSquareEquation a b c .
+                  --   a x² + b x + x == 0
+                  conjoin
+                    [ counterexample ("err = " ++ show err) $
+                        err < accuracy
+                    | x <- roots
+                    , let err =
+                            (a * x * x + b * x + c)
+                            / maximum (map abs [a, b, c])
+                    ]
       ]
+
+accuracy = 1e-12
