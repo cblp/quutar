5def4737693b70057156261a3afa0066b07b0c28
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-28 19:15:04 +0300

    05-web: Add form

--- 05-web/server/Main.hs
+++ 05-web/server/Main.hs
@@ -1,10 +1,19 @@
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Web.Scotty (get, html, param, scotty)
+import           GHC.Generics    (Generic)
+import           Options.Generic (ParseRecord, getRecord)
+import           Web.Scotty      (get, html, scotty)
+
+newtype Options = Options{port :: Int}
+  deriving (Generic, ParseRecord)
 
 main :: IO ()
-main =
-  scotty 3000 $
-    get "/:word" $ do
-      beam <- param "word"
-      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+main = do
+  Options{port} <- getRecord "A simple web server"
+  putStrLn $ "Listening at http://localhost:" ++ show port
+  scotty port $
+    get "/" $ do
+      html "<form><input name=expr></form>"
