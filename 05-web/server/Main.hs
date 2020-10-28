c922a210f34ca653d4123be402fb878ab52b6e00
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-28 20:18:30 +0300

    05: Add calc

--- 05-web/server/Main.hs
+++ 05-web/server/Main.hs
@@ -2,10 +2,17 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
-import           GHC.Generics    (Generic)
-import           Options.Generic (ParseRecord, getRecord)
-import           Web.Scotty      (get, html, scotty)
+import           Control.Applicative   ((<|>))
+import qualified Data.Text             as Text
+import           GHC.Generics          (Generic)
+import           Options.Generic       (ParseRecord, getRecord)
+import           Text.Read             (readEither)
+import           Text.Shakespeare.Text (stext)
+import           Web.Scotty            (ActionM, get, html, param, scotty)
+
+import           Expr                  (eval)
 
 newtype Options = Options{port :: Int}
   deriving (Generic, ParseRecord)
@@ -14,6 +21,37 @@ main :: IO ()
 main = do
   Options{port} <- getRecord "A simple web server"
   putStrLn $ "Listening at http://localhost:" ++ show port
+  server port
+
+server :: Int -> IO ()
+server port =
   scotty port $
-    get "/" $ do
-      html "<form><input name=expr></form>"
+    get "/" form
+
+form :: ActionM ()
+form = do
+  exprText <- param "expr"
+  mx       <- (Just <$> param "x") <|> pure Nothing
+  let eExpr = readEither $ Text.unpack exprText
+  let evalResult =
+        case (eExpr, mx) of
+          (Left err, _)     -> err
+          (_, Nothing)      -> ""
+          (Right e, Just x) -> show $ eval e x
+  html
+    [stext|
+      <form>
+        <table>
+          <tr>
+            <td>expr
+            <td><input name=expr value="#{exprText}">
+            <td>#{either id show eExpr}
+          <tr>
+            <td>x
+            <td><input name=x value="#{maybe "" show mx}">
+          <tr>
+            <td>eval
+            <td>#{evalResult}
+        </table>
+        <input type=submit>
+    |]
