ee9a4f1a1c086222f547eaf261d0bbb107478d09
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-31 17:42:02 +0300

    05 web server: Add lecture conspect

--- 05-web/server/Main.hs
+++ 05-web/server/Main.hs
@@ -5,56 +5,75 @@
 {-# LANGUAGE QuasiQuotes       #-}
 
 import           Control.Applicative   ((<|>))
-import qualified Data.Text             as Text
+import           Control.Monad         (when)
 import           GHC.Generics          (Generic)
 import           Options.Generic       (ParseRecord, getRecord)
-import           Text.Read             (readEither)
+import           Text.Read             (readMaybe)
 import           Text.Shakespeare.Text (stext)
-import           Web.Scotty            (ActionM, get, html, param, scotty)
+import           Web.Scotty            (ActionM, ScottyM, get, html, param,
+                                        scotty)
 
 import           Expr                  (eval)
 
-newtype Options = Options{port :: Int}
+data Options = Options
+  { port    :: Int
+  , verbose :: Bool
+  }
   deriving (Generic, ParseRecord)
+
+-- when condition action =
+--   if condition then
+--     action
+--   else
+--     pure ()
 
 main :: IO ()
 main = do
-  Options{port} <- getRecord "A simple web server"
-  putStrLn $ "Listening at http://localhost:" ++ show port
-  server port
+  Options{port, verbose} <- getRecord "A simple web server"
+  when verbose $
+    putStrLn $ "Starting server at http://localhost:" ++ show port
+  scotty port serverDescription
 
-server :: Int -> IO ()
-server port =
-  scotty port $
-    get "/" form
+serverDescription :: ScottyM ()
+serverDescription =
+  get "/" form
 
 form :: ActionM ()
 form = do
-  exprText <- param "expr" <|> pure ""
-  mx       <- (Just <$> param "x") <|> pure Nothing
-  let
-    eExpr =
-      case exprText of
-        "" -> Left ""
-        _  -> readEither $ Text.unpack exprText
-    evalResult =
-      case (eExpr, mx) of
-        (Right e, Just x) -> show $ eval e x
-        _                 -> ""
+  pExpr <- param "expr" <|> pure ""
+  pX    <- param "x"    <|> pure ""
+  let mResult = do
+        x    <- readMaybe pX
+        expr <- readMaybe pExpr
+        pure $ eval expr x
   html
     [stext|
       <form>
         <table>
-          <tr>
-            <td>expr:
-            <td><input name=expr value="#{exprText}">
-            <td>#{either id show eExpr}
-          <tr>
-            <td>x:
-            <td><input name=x value="#{maybe "" show mx}">
-          <tr>
-            <td>eval:
-            <td>#{evalResult}
+          <tr><td>expr: <td><input name=expr value="#{pExpr}">
+          <tr><td>x:    <td><input name=x value="#{pX}">
+          <tr><td>eval: <td>#{show mResult}
         </table>
         <input type=submit>
     |]
+
+-- beam :: ActionM ()
+-- beam = do
+--   word <- param "word"
+--   html $ mconcat ["Scotty, ", word, " me up!"]
+
+--------------------------
+
+-- -- bind
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+
+-- getConfig >>= getPort >>= getNetworkData
+
+-- ma >>= f = do a <- ma
+--               f a
+
+-- (>>) :: Monad m => m a -> m b -> m b
+-- ma >> mb = ma >>= (\_ -> mb)
+--   == do
+--         ma
+--         mb
