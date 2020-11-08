75f17b4a57f748ae9e94be69d2114f2c458e81fc
Yuriy Syrovetskiy <cblp@cblp.su>
2020-11-08 11:53:38 +0300

    06 parsing: Add task

--- 06-parsing/lib/Expr/Parse.hs
+++ 06-parsing/lib/Expr/Parse.hs
@@ -1,25 +1,49 @@
 module Expr.Parse (expr) where
 
-import           Control.Applicative        ((<|>))
-import           Data.Foldable              (asum)
-import           Data.Void                  (Void)
-import           Text.Megaparsec            (Parsec, try)
-import           Text.Megaparsec.Char       (char, space)
-import           Text.Megaparsec.Char.Lexer (decimal, float, signed)
+import           Prelude                        hiding (cos, div, sin)
 
-import           Expr                       (Expr (..))
+import           Control.Applicative            ((<|>))
+import           Control.Monad.Combinators.Expr (Operator (..), makeExprParser)
+import           Data.Foldable                  (asum)
+import           Data.Functor                   (($>))
+import           Data.Void                      (Void)
+import           Text.Megaparsec                (Parsec, try)
+import           Text.Megaparsec.Char           (char, space,
+                                                --  string
+                                                 )
+import           Text.Megaparsec.Char.Lexer     (decimal, float, signed)
+
+import           Expr                           (Expr (..))
 
 type Parser = Parsec Void String
 
 expr :: Parser Expr
 expr =
-  asum
-    [ try number
-    , try var
+  makeExprParser
+    term
+    [ [Postfix $ try pow]
+    , [InfixL $ try add]
     ]
+
+term :: Parser Expr
+term = space *> asum [number, var {- , expr в скобках -}]
 
 number :: Parser Expr
 number = Number <$> signed space (try float <|> decimal)
 
+integer :: Parser Integer
+integer = signed space decimal
+
 var :: Parser Expr
-var = Var <$ char 'x'
+var = space *> char 'x' $> Var
+
+pow :: Parser (Expr -> Expr)
+pow = do
+  space
+  _ <- char '^'
+  space
+  n <- integer
+  pure $ \e -> Pow e n
+
+add :: Parser (Expr -> Expr -> Expr)
+add = space *> char '+' $> Add
--- 06-parsing/test/Main.hs
+++ 06-parsing/test/Main.hs
@@ -1,9 +1,13 @@
-import           Test.Tasty       (TestName, TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (assertFailure, testCase, (@?=))
-import           Text.Megaparsec  (errorBundlePretty, parse)
+import           Test.Tasty            (TestName, TestTree, defaultMain,
+                                        testGroup)
+import           Test.Tasty.HUnit      (assertFailure, testCase, (@?=))
+import           Test.Tasty.QuickCheck (Arbitrary, arbitrary, getSize, oneof,
+                                        -- counterexample, testProperty, (===)
+                                        scale)
+import           Text.Megaparsec       (errorBundlePretty, parse)
 
-import           Expr             (Expr (..))
-import           Expr.Parse       (expr)
+import           Expr                  (Expr (..))
+import           Expr.Parse            (expr)
 
 main :: IO ()
 main =
@@ -13,6 +17,12 @@ main =
       , caseParseExpr "negative integer" "-4"    $ Number (-4)
       , caseParseExpr "floating"         "-4.18" $ Number (-4.18)
       , caseParseExpr "var"              "x"       Var
+      -- , testProperty "show <-> parse" $
+      --   \e ->
+      --     case parse expr "show" (show e) of
+      --       Left errorBundle ->
+      --         counterexample (errorBundlePretty errorBundle) False
+      --       Right e' -> e' === e
       ]
 
 caseParseExpr :: TestName -> String -> Expr -> TestTree
@@ -21,3 +31,23 @@ caseParseExpr description input expected =
     case parse expr "input" input of
       Left errorBundle -> assertFailure $ errorBundlePretty errorBundle
       Right e          -> e @?= expected
+
+instance Arbitrary Expr where
+  arbitrary = do
+    size <- getSize
+    oneof $ simple ++ if size >= 1 then complex else []
+    where
+      halve = scale (`div` 2)
+      simple =
+        [ pure Var
+        , Number <$> oneof [arbitrary, fromInteger <$> arbitrary]
+        ]
+      complex =
+        [ Sin <$> arbitrary
+        , Cos <$> arbitrary
+        , Pow <$> halve arbitrary <*> halve arbitrary
+        , Mul <$> halve arbitrary <*> halve arbitrary
+        , Div <$> halve arbitrary <*> halve arbitrary
+        , Add <$> halve arbitrary <*> halve arbitrary
+        , Sub <$> halve arbitrary <*> halve arbitrary
+        ]
