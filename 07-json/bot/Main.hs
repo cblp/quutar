92687cf68209686a66e27d8e080a7195b0843002
Yuriy Syrovetskiy <cblp@cblp.su>
2020-11-15 17:47:37 +0300

    07-json: Extract Telegram-specific things to a library

--- 07-json/bot/Main.hs
+++ 07-json/bot/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -8,24 +6,21 @@ module Main where
 import           Prelude            hiding (id)
 
 import           Control.Exception  (catchJust)
-import           Control.Lens       ((^.))
 import           Control.Monad      (guard, void)
-import           Data.Aeson         as Json
-import           Data.Foldable      (for_, toList)
+import           Data.Foldable      (for_)
 import           Data.Maybe         (fromMaybe)
-import           Data.Text          (Text)
 import qualified Data.Text          as Text
 import           Data.Time          (getCurrentTime)
-import           Database.Persist   (Entity (..), selectList, upsert, (=.))
-import           GHC.Generics       (Generic)
-import           Network.Wreq       (FormParam ((:=)), asJSON, post,
-                                     responseBody)
+import           Database.Persist   (entityVal, selectList, upsert, (=.))
 import           System.Environment (getEnv)
 import           System.IO          (hPutStr, stderr)
 import           System.IO.Error    (isDoesNotExistError)
 import           Text.Read          (readMaybe)
 
 import           Database           (EntityField (StakeText), Stake (..), runDb)
+import           Telegram           (Chat (..), Message (..), Token,
+                                     Update (..), User (..), getUpdates,
+                                     sendMessage)
 
 main :: IO ()
 main = do
@@ -78,85 +73,6 @@ loadUpdateId =
     (guard . isDoesNotExistError)
     (readMaybe <$> readFile "update_id")
     (\_ -> pure Nothing)
-
-type Token = String
-
-data Update = Update
-  { update_id :: Integer
-  , message   :: Message
-  }
-  deriving (FromJSON, Generic)
-
--- instance FromJSON Update where
---   parseJSON =
---     withObject "Update" $
---       \obj -> do
---         update_id <- obj .: "update_id"
---         message   <- obj .: "message"
---         pure Update{update_id, message}
-
-data Message = Message
-  { chat :: Chat
-  , text :: Maybe Text
-  , from :: User
-  }
-  deriving (FromJSON, Generic, Show)
-
-data User = User
-  { first_name :: Text
-  , username   :: Maybe Text
-  }
-  deriving (FromJSON, Generic, Show)
-
-data Chat = Chat
-  { id :: Integer
-  }
-  deriving (FromJSON, Generic, Show)
-
-data Ok a = Ok
-  { ok     :: Bool
-  , result :: a
-  }
-  deriving (FromJSON, Generic)
-
--- instance FromJSON Bool...
-
--- instance FromJSON a => FromJSON (Ok a) where
---   parseJSON =
---     withObject "Ok" $
---       \obj -> do
---         ok     <- obj .: "ok"
---         result <- obj .: "result"
---         pure Ok{ok, result}
-
---           case HashMap.lookup "ok" obj of
---             Nothing -> fail "no key 'ok'"
---             Just v ->
---               -- case v of
---               --   Json.Bool b -> pure b
---               --   _           -> fail "not a bool"
---               parseJSON v
-
-getUpdates :: Token -> Maybe Integer -> IO [Update]
-getUpdates token lastUpdateIdM =
-  telegramRpc token "getUpdates" params
-  where
-    params =
-      ("timeout" := (20 :: Integer))
-      : ["offset" := updateId + 1 | updateId <- toList lastUpdateIdM]
-
-sendMessage :: Token -> Integer -> Text -> IO Message
-sendMessage token chat_id text =
-  telegramRpc token "sendMessage" ["chat_id" := chat_id, "text" := text]
-
-telegramRpc :: FromJSON result => Token -> String -> [FormParam] -> IO result
-telegramRpc token method params = do
-  rawResponse <-
-    post ("https://api.telegram.org/bot" <> token <> "/" <> method) params
-  jsonResponse <- asJSON rawResponse
-  let Ok{ok, result} = jsonResponse ^. responseBody
-  guard ok
-  pure result
 
 putLog :: String -> IO ()
 putLog message = do
--- 07-json/telegram/Telegram.hs
+++ 07-json/telegram/Telegram.hs
@@ -1,1 +1,96 @@
-module Telegram () where
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Telegram
+  ( Chat (..), Message (..), Token, Update (..), User (..)
+  , getUpdates, sendMessage
+  ) where
+
+import           Control.Lens  ((^.))
+import           Control.Monad (guard)
+import           Data.Aeson    (FromJSON)
+import           Data.Foldable (toList)
+import           Data.Text     (Text)
+import           GHC.Generics  (Generic)
+import           Network.Wreq  (FormParam ((:=)), asJSON, post, responseBody)
+
+type Token = String
+
+data Update = Update
+  { update_id :: Integer
+  , message   :: Message
+  }
+  deriving (FromJSON, Generic)
+
+-- instance FromJSON Update where
+--   parseJSON =
+--     withObject "Update" $
+--       \obj -> do
+--         update_id <- obj .: "update_id"
+--         message   <- obj .: "message"
+--         pure Update{update_id, message}
+
+data Message = Message
+  { chat :: Chat
+  , text :: Maybe Text
+  , from :: User
+  }
+  deriving (FromJSON, Generic, Show)
+
+data User = User
+  { first_name :: Text
+  , username   :: Maybe Text
+  }
+  deriving (FromJSON, Generic, Show)
+
+data Chat = Chat
+  { id :: Integer
+  }
+  deriving (FromJSON, Generic, Show)
+
+data Ok a = Ok
+  { ok     :: Bool
+  , result :: a
+  }
+  deriving (FromJSON, Generic)
+
+-- instance FromJSON Bool...
+
+-- instance FromJSON a => FromJSON (Ok a) where
+--   parseJSON =
+--     withObject "Ok" $
+--       \obj -> do
+--         ok     <- obj .: "ok"
+--         result <- obj .: "result"
+--         pure Ok{ok, result}
+
+--           case HashMap.lookup "ok" obj of
+--             Nothing -> fail "no key 'ok'"
+--             Just v ->
+--               -- case v of
+--               --   Json.Bool b -> pure b
+--               --   _           -> fail "not a bool"
+--               parseJSON v
+
+getUpdates :: Token -> Maybe Integer -> IO [Update]
+getUpdates token lastUpdateIdM =
+  telegramRpc token "getUpdates" params
+  where
+    params =
+      ("timeout" := (20 :: Integer))
+      : ["offset" := updateId + 1 | updateId <- toList lastUpdateIdM]
+
+sendMessage :: Token -> Integer -> Text -> IO Message
+sendMessage token chat_id text =
+  telegramRpc token "sendMessage" ["chat_id" := chat_id, "text" := text]
+
+telegramRpc :: FromJSON result => Token -> String -> [FormParam] -> IO result
+telegramRpc token method params = do
+  rawResponse <-
+    post ("https://api.telegram.org/bot" <> token <> "/" <> method) params
+  jsonResponse <- asJSON rawResponse
+  let Ok{ok, result} = jsonResponse ^. responseBody
+  guard ok
+  pure result
