dc4a5dc4599a76285964c0cacd81cfc15a77cbbc
Yuriy Syrovetskiy <cblp@cblp.su>
2020-12-01 00:29:28 +0300

    Use citems

--- 09-compiler/Main.hs
+++ 09-compiler/Main.hs
@@ -3,7 +3,7 @@
 import           Prelude hiding (filter)
 
 import qualified Language.C.Quote as C
-import           Language.C.Quote.GCC (cdecl, cexp, cstms, cunit)
+import           Language.C.Quote.GCC (citems, cexp, cstms, cunit)
 import           System.Process (callProcess)
 import           Text.PrettyPrint.Mainland (pretty)
 import           Text.PrettyPrint.Mainland.Class (ppr)
@@ -32,8 +32,8 @@ genC (Program filter aggregate) =
     $esc:("#include <string.h>")
 
     int main() {
-      $decl:filterDeclaration;
-      $decl:aggregateDeclaration;
+      $items:filterDeclarations
+      $items:aggregateDeclarations
       typename int64_t value;
       while (
         scanf($string:(filterScanfSpec ++ "%lld"), $filterScanfRef, &value) == 2
@@ -51,60 +51,52 @@ genC (Program filter aggregate) =
     AggregateCode{..} = compileAggregate aggregate
 
 data FilterCode = FilterCode
-  { filterDeclaration :: C.InitGroup
-  , filterScanfSpec   :: String
-  , filterScanfRef    :: C.Exp
-  , filterCondition   :: C.Exp
+  { filterDeclarations :: [C.BlockItem]
+  , filterScanfSpec    :: String
+  , filterScanfRef     :: C.Exp
+  , filterCondition    :: C.Exp
   }
 
 compileFilter :: Filter -> FilterCode
 compileFilter = \case
   FilterString s ->
     FilterCode
-      { filterDeclaration = [cdecl| char key[16]; |]
-      , filterScanfSpec   = "%s"
-      , filterScanfRef    = [cexp| key |]
-      , filterCondition   = [cexp| strcmp(key, $string:s) == 0 |]
+      { filterDeclarations = [citems| char key[16]; |]
+      , filterScanfSpec    = "%s"
+      , filterScanfRef     = [cexp| key |]
+      , filterCondition    = [cexp| strcmp(key, $string:s) == 0 |]
       }
   FilterInt i ->
     FilterCode
-      { filterDeclaration = [cdecl| typename int64_t key; |]
-      , filterScanfSpec   = "%lld"
-      , filterScanfRef    = [cexp| &key |]
-      , filterCondition   = [cexp| key == $int:i |]
+      { filterDeclarations = [citems| typename int64_t key; |]
+      , filterScanfSpec    = "%lld"
+      , filterScanfRef     = [cexp| &key |]
+      , filterCondition    = [cexp| key == $int:i |]
       }
 
 data AggregateCode = AggregateCode
-  { aggregateDeclaration :: C.InitGroup
-  , aggregateStatements  :: [C.Stm]
-  , aggregateResult      :: C.Exp
+  { aggregateDeclarations :: [C.BlockItem]
+  , aggregateStatements   :: [C.Stm]
+  , aggregateResult       :: C.Exp
   }
 
 compileAggregate :: Aggregate -> AggregateCode
 compileAggregate = \case
   Sum ->
     AggregateCode
-      { aggregateDeclaration = [cdecl| typename int64_t result = 0; |]
-      , aggregateStatements  = [cstms| result += value; |]
-      , aggregateResult      = [cexp| result |]
+      { aggregateDeclarations = [citems| typename int64_t sum = 0; |]
+      , aggregateStatements   = [cstms| sum += value; |]
+      , aggregateResult       = [cexp| sum |]
       }
   Min ->
     AggregateCode
-      { aggregateDeclaration = [cdecl| typename int64_t result = INT64_MAX; |]
-      , aggregateStatements  =
-          [cstms|
-            if (value < result)
-              result = value;
-          |]
-      , aggregateResult      = [cexp| result |]
+      { aggregateDeclarations = [citems| typename int64_t min = INT64_MAX; |]
+      , aggregateStatements   = [cstms| if (value < min) min = value; |]
+      , aggregateResult       = [cexp| min |]
       }
   Max ->
     AggregateCode
-      { aggregateDeclaration = [cdecl| typename int64_t result = INT64_MIN; |]
-      , aggregateStatements  =
-          [cstms|
-            if (value > result)
-              result = value;
-          |]
-      , aggregateResult      = [cexp| result |]
+      { aggregateDeclarations = [citems| typename int64_t max = INT64_MIN; |]
+      , aggregateStatements   = [cstms| if (value > max) max = value; |]
+      , aggregateResult       = [cexp| max |]
       }
