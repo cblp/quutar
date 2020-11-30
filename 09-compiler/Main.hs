d596d6c7c643b0fd02c390b182175a52c181084f
Yuriy Syrovetskiy <cblp@cblp.su>
2020-11-30 23:16:38 +0300

    Add FilterCode, compileFilter

--- 09-compiler/Main.hs
+++ 09-compiler/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 import           Prelude hiding (filter)
 
 import qualified Language.C.Quote as C
@@ -6,14 +8,10 @@ import           System.Process (callProcess)
 import           Text.PrettyPrint.Mainland (pretty)
 import           Text.PrettyPrint.Mainland.Class (ppr)
 
-data Program = Program
-  { filter  :: Filter
-  , mapper  :: Mapper
-  , reducer :: Reducer
-  }
+data Program = Program Filter Mapper Reducer
 
 data Filter = Filter
-  { filterValue :: FilterValue
+  { value :: FilterValue
   -- TODO , filterField :: Word
   }
 
@@ -31,8 +29,8 @@ main :: IO ()
 main =
   compileAndWriteFile "filtersum.c" $
     Program
-      (Filter {- 1 -} $ FilterInt 33)
-      (Mapper {- 2 -})
+      Filter{ {- field = 1 -} value = FilterInt 33}
+      Mapper{ {- field = 2 -} }
       Sum
 
 compileAndWriteFile :: FilePath -> Program -> IO ()
@@ -42,7 +40,7 @@ compileAndWriteFile file prog = do
     "gcc" [file, "-Wall", "-Werror", "-Wextra", "-pedantic"]
 
 compile :: Program -> [C.Definition]
-compile Program{filter = Filter{filterValue}} =
+compile (Program filter Mapper Sum) =
   [cunit|
     $esc:("#include <stdint.h>")
     $esc:("#include <stdio.h>")
@@ -50,10 +48,10 @@ compile Program{filter = Filter{filterValue}} =
 
     int main() {
       typename int64_t sum = 0;
-      $decl:decl1;
+      $decl:declaration;
       typename int64_t f2;
-      while (scanf($string:scanSpec1, $scanArg1, &f2) == 2) {
-        if ($filterCond)
+      while (scanf($string:scanfSpec, $scanfRef, &f2) == 2) {
+        if ($condition)
           sum += f2;
       }
       printf("%lld\n", sum);
@@ -61,17 +59,30 @@ compile Program{filter = Filter{filterValue}} =
     }
   |]
   where
-    (decl1, scanSpec1, scanArg1, filterCond) =
-      case filterValue of
-        FilterString s ->
-          ( [cdecl| char f1[16]; |]
-          , "%s%lld"
-          , [cexp| f1 |]
-          , [cexp| strcmp(f1, $string:s) == 0 |]
-          )
-        FilterInt i ->
-          ( [cdecl| typename int64_t f1; |]
-          , "%lld%lld"
-          , [cexp| &f1 |]
-          , [cexp| f1 == $int:i |]
-          )
+    FilterCode{declaration, scanfSpec, scanfRef, condition} =
+      compileFilter filter
+
+data FilterCode = FilterCode
+  { declaration :: C.InitGroup
+  , scanfSpec   :: String
+  , scanfRef    :: C.Exp
+  , condition   :: C.Exp
+  }
+
+compileFilter :: Filter -> FilterCode
+compileFilter Filter{value} =
+  case value of
+    FilterString s ->
+      FilterCode
+        { declaration = [cdecl| char f1[16]; |]
+        , scanfSpec   = "%s%lld"
+        , scanfRef    = [cexp| f1 |]
+        , condition   = [cexp| strcmp(f1, $string:s) == 0 |]
+        }
+    FilterInt i ->
+      FilterCode
+        { declaration = [cdecl| typename int64_t f1; |]
+        , scanfSpec   = "%lld%lld"
+        , scanfRef    = [cexp| &f1 |]
+        , condition   = [cexp| f1 == $int:i |]
+        }
