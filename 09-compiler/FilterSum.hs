8669bf3dac1c8bff59702867a17a75e8d1f14d51
Yuriy Syrovetskiy <cblp@cblp.su>
2020-12-01 11:39:46 +0300

    fixup! fixup! Add CI for 09-compiler

--- 09-compiler/FilterSum.hs
+++ 09-compiler/FilterSum.hs
@@ -5,8 +5,9 @@ main = do
 
 readRecord :: String -> (String, Integer)
 readRecord s =
-  let i1 : i2 : _ = words s
-  in (i1, read i2)
+  case words s of
+    i1 : i2 : _ -> (i1, read i2)
+    _           -> error "there must be at least 2 fields in a row"
 
 filterSum :: String -> [(String, Integer)] -> Integer
 filterSum x = sum . map snd . filter (\(x', _) -> x' == x)
